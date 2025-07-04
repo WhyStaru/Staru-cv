f7bfdb43bd741eff2b379f0538c69ff94bd1083f
Whystaru <ilhambintangs12345@gmail.com>
2025-07-04 15:00:58 +0700

    cv

---
+++ backend/data.js
@@ -0,0 +1,7 @@
+const educationHistory = [
+    { id: 1, period: '2022 - Sekarang', institution: 'Universitas Gadjah Mada', major: 'S1 - Teknik Informatika' },
+    { id: 2, period: '2019 - 2022', institution: 'SMA Negeri 1 Teladan Yogyakarta', major: 'MIPA' }
+    ];
+const skills = [ /* ...data... */ ];
+const projects = [ /* ...data... */ ];
+module.exports = { educationHistory, skills, projects };
